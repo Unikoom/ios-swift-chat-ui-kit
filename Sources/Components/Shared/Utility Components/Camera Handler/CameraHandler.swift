8bb6304d4fe4d79d75a716eb3bc526fcd1051132
Stanislav Redreiev <stanislav.redreiev@gmail.com>
2023-06-06 17:16:06 +0300

    ~ update image orientation for picture from camera
    ~ fix error
    
    ~
    
    ~
    
    ~
    
    ~

--- Sources/Components/Shared/Utility Components/Camera Handler/CameraHandler.swift
+++ Sources/Components/Shared/Utility Components/Camera Handler/CameraHandler.swift
@@ -75,10 +75,13 @@ extension CameraHandler: UIImagePickerControllerDelegate, UINavigationController
                 self.imagePickedBlock?(imageURL.absoluteString ?? "")
             }
         case .camera:
-            guard let image = info[.originalImage] as? UIImage else {
+            guard
+                let image = info[.originalImage] as? UIImage
+            else {
                 return
             }
-            saveImage(imageName: "image_\(Int(Date().timeIntervalSince1970 * 100)).png", image: image)
+            let fixedImage = image.fixOrientation()
+            saveImage(imageName: "image_\(Int(Date().timeIntervalSince1970 * 100)).png", image: fixedImage)
         case .savedPhotosAlbum:
             self.imagePickedBlock?("\(String(describing: info[UIImagePickerController.InfoKey.imageURL]!))")
         @unknown default:
@@ -125,4 +128,71 @@ extension CameraHandler: UIImagePickerControllerDelegate, UINavigationController
         return nil
     }
     
+}
+
+
+public extension UIImage {
+    
+    /// Extension to fix orientation of an UIImage without EXIF
+    func fixOrientation() -> UIImage {
+        
+        guard let cgImage = cgImage else { return self }
+        
+        if imageOrientation == .up { return self }
+        
+        var transform = CGAffineTransform.identity
+        
+        switch imageOrientation {
+            
+        case .down, .downMirrored:
+            transform = transform.translatedBy(x: size.width, y: size.height)
+            transform = transform.rotated(by: CGFloat(Double.pi))
+            
+        case .left, .leftMirrored:
+            transform = transform.translatedBy(x: size.width, y: 0)
+            transform = transform.rotated(by: CGFloat(Double.pi/2))
+            
+        case .right, .rightMirrored:
+            transform = transform.translatedBy(x: 0, y: size.height)
+            transform = transform.rotated(by: CGFloat(-Double.pi/2))
+            
+        case .up, .upMirrored:
+            break
+        }
+        
+        switch imageOrientation {
+            
+        case .upMirrored, .downMirrored:
+            transform.translatedBy(x: size.width, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+            
+        case .leftMirrored, .rightMirrored:
+            transform.translatedBy(x: size.height, y: 0)
+            transform.scaledBy(x: -1, y: 1)
+            
+        case .up, .down, .left, .right:
+            break
+        }
+        
+        if let ctx = CGContext(data: nil, width: Int(size.width), height: Int(size.height), bitsPerComponent: cgImage.bitsPerComponent, bytesPerRow: 0, space: cgImage.colorSpace!, bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue) {
+            
+            ctx.concatenate(transform)
+            
+            switch imageOrientation {
+                
+            case .left, .leftMirrored, .right, .rightMirrored:
+                ctx.draw(cgImage, in: CGRect(x: 0, y: 0, width: size.height, height: size.width))
+                
+            default:
+                ctx.draw(cgImage, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            }
+            
+            if let finalImage = ctx.makeImage() {
+                return (UIImage(cgImage: finalImage))
+            }
+        }
+        
+        // something failed -- return original
+        return self
+    }
 }
